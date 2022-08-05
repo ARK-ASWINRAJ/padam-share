4e7fdbdc7d94021a3588ca5014d6c5000d8d39d5
aswinraj <aswinrajkailath@gmail.com>
2022-08-05 22:06:15 +0530

    Added Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-blue-500 font-bold underline">
-        Padam Share is Online.
-      </h1>
+      <Head>
+        <title>Padam Share</title>
+      </Head>
+      <Navbar />
+      <div className="items-center text-center">
+        <h1 className="text-8xl text-transparent bg-clip-text mx-auto max-w-[650px] bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 md:mt-64 mt-32 font-bold ">
+          Padam Share
+        </h1>
+        <p className="text-4xl mx-auto max-w-[400px] mt-8 font-semibold ">
+          Is Online
+        </p>
+      </div>
     </>
   );
 }
